bdfd057ef69ce9e5c0884c6b632163efda4b7e94
Cristina Sanz <cristina.sanz508@comunidadunir.net>
2025-12-13 18:02:15 +0100

    Creamos el servicio de HTTPCLient y cargaos los productos

---
+++ src/app/services/product.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+export interface Product {
+  _id: string;
+  name: string;
+  description: string;
+  price: number;
+  category: string;
+  image: string;
+  active: boolean;
+}
+
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ProductService {
+  //recoge el modelo (interfaz de tipo Product) que tranforma en un array de productos
+  private url = 'https://api.npoint.io/1dee63ad8437c82b24fe'
+
+  constructor(private http: HttpClient) {
+  }
+
+  //metodo para cargar productos
+  cargarProductos() {
+    return this.http.get<Product[]>(this.url)
+  }
+}
